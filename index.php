22a445e60e716449763dbb6806141c2c6f0bdde5
Marcial <miguelrojasmarcy@gmail.com>
2022-10-14 15:04:18 -0500

    ola

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=0.1">
+    <meta name="description" content="Felicidades NanCy">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feliz Cumpleaños</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+       <nav>
+       </nav>
+     <section class="textos-header">
+       <h1>FELIZ CUMPLEAÑOS PANA </h1>
+       <h2>Que en tu vida la felicidad sea constante y nunca te falten la salud y el amor. </h2>
+     </section>
+       <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C267.77,202.78 344.52,-45.88 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
+    </header>
+    
+    <main>
+     <section class="contenedor sobre-nosotros">
+
+       <div class="col-auto p-5 text-center">
+        <h1 class="titulo">Hoy cumples un año más en tu vida </h1><br>
+          <img src="img/01.jpg" alt="" class="imagen-about us" width="500" height="640">
+          
+       </div>
+    
+          <div class="contenedor-sobre-nosotros">
+           
+            <div class="contenido-textos">
+                <h3>
+                  Pana...!!!!<3 
+                </h3>
+                <P>Estas son las mañanitas<br>
+Que cantaba el rey David<br>
+Hoy por ser día de tu santo<br>
+Te las cantamos aquí <br><br>
+Despierta, mi bien, despierta<br>
+Mira que ya amaneció<br>
+Ya los pajaritos cantan<br>
+La luna ya se metió. <br>
+Fin
+<br>
+<br>
+<hr>
+Muchas felicidades Pana, hoy por ser el día de tu cumpleaños, espero que te la pases de lo mejor hoy y siempre  <br>
+Que se cumplan todos tus sueños y metas que te has planteado. Échale muchas ganas a la escuela que siempre salgas bien en todo y si no es el caso, no te deprimas, es parte del aprendizaje, por que para sabernos levantar primero debemos de caer. 
+Que siempre tengas salud, paz y amor. <br><br>
+Te mando un fuerte abrazo desde aqui hasta allá donde tus andas. <br>
+
+<hr>
+Te Quiero Mucho :) ...!
+<div class="col-auto p-5 text-center">
+          <img src="img/21.jpg" alt="" class="imagen-about us" width="800" height="400">
+          
+</div>           
+                  
+                  
+                  <br><br><br>
+      
+      
+            
+               
+            </div>
+          </div>
+     </section>
+
+    </main>
+
+
+  <footer class="text-center text-white" style="background-color: #45637d;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Iframe -->
+
+    <!-- Section: Iframe -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    @Ing. Marcial Miguel Rojas
+  </div>
+  <!-- Copyright -->
+</footer>
+  
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
+
